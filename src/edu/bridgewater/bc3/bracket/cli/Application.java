c7dd7a7afdc990e7eed7c57d59ae08db32692a90
5donuts <charliegerman32@gmail.com>
2017-01-07 17:55:39 -0500

    added skeleton for CLI application

---
+++ src/edu/bridgewater/bc3/bracket/cli/Application.java
@@ -0,0 +1,12 @@
+package edu.bridgewater.bc3.bracket.cli;
+
+/**
+ * Because I'm lazy, I'm developing a CLI version of this application first,
+ * then we can do the whole fancy GUI thing
+ * 
+ * @author Charles German
+ *
+ */
+public class Application {
+
+}
